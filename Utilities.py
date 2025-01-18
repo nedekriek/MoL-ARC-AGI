a7a7b086d75e6d7b67aae99f2a5af4674d470c19
Luxemburgli <luxemburglee@outlook.com>
2025-01-18 01:24:39 +0100

    Update Utilities.py

---
+++ Utilities.py
@@ -0,0 +1,92 @@
+### Here are some util functions. They are mainly taken from publicly shared notebook by Somayyeh Gholami & Mehran Kazeminia containing solutions from previous ARC competition on Kaggle: https://www.kaggle.com/code/mehrankazeminia/3-arc24-developed-2020-winning-solutions.
+
+#####################################################
+
+import numpy as np
+import matplotlib
+from matplotlib import colors
+import matplotlib.pyplot as plt
+
+### Import the packages
+
+#####################################################
+
+### Define the colors
+
+cmap = colors.ListedColormap(
+    ['#000000', '#0074D9','#FF4136','#2ECC40','#FFDC00',
+     '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25'])
+
+norm = colors.Normalize(vmin=0, vmax=9)
+color_list = ["black", "blue", "red", "green", "yellow", "gray", "magenta", "orange", "sky", "brown"]
+
+###
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+def plot_pic(x):
+    plt.imshow(np.array(x), cmap=cmap, norm=norm)
+    plt.show()
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+def plot_data(task):
+    n = len(task["train"]) + len(task["test"])
+    fig, axs = plt.subplots(2, n, figsize=(4*n,8), dpi=200)
+    plt.subplots_adjust(wspace=0, hspace=0)
+    fig_num = 0
+    for i, t in enumerate(task["train"]):
+        t_in, t_out = np.array(t["input"]), np.array(t["output"])
+        axs[0][fig_num].imshow(t_in, cmap=cmap, norm=norm)
+        axs[0][fig_num].set_title(f'Train-{i} in')
+        axs[0][fig_num].set_yticks(list(range(t_in.shape[0])))
+        axs[0][fig_num].set_xticks(list(range(t_in.shape[1])))
+        axs[1][fig_num].imshow(t_out, cmap=cmap, norm=norm)
+        axs[1][fig_num].set_title(f'Train-{i} out')
+        axs[1][fig_num].set_yticks(list(range(t_out.shape[0])))
+        axs[1][fig_num].set_xticks(list(range(t_out.shape[1])))
+        fig_num += 1
+    for i, t in enumerate(task["test"]):
+        t_in, t_out = np.array(t["input"]), np.array(t["output"])
+        axs[0][fig_num].imshow(t_in, cmap=cmap, norm=norm)
+        axs[0][fig_num].set_title(f'Test-{i} in')
+        axs[0][fig_num].set_yticks(list(range(t_in.shape[0])))
+        axs[0][fig_num].set_xticks(list(range(t_in.shape[1])))
+        axs[1][fig_num].imshow(t_out, cmap=cmap, norm=norm)
+        axs[1][fig_num].set_title(f'Test-{i} out')
+        axs[1][fig_num].set_yticks(list(range(t_out.shape[0])))
+        axs[1][fig_num].set_xticks(list(range(t_out.shape[1])))
+        fig_num += 1
+    
+    plt.tight_layout()
+    plt.show()
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+def plot_task(task):
+    n = len(task["train"]) + len(task["test"])
+    fig, axs = plt.subplots(2, n, figsize=(4*n,8), dpi=200)
+    plt.subplots_adjust(wspace=0, hspace=0)
+    fig_num = 0
+    for i, t in enumerate(task["train"]):
+        t_in, t_out = np.array(t["input"]), np.array(t["output"])
+        axs[0][fig_num].imshow(t_in, cmap=cmap, norm=norm)
+        axs[0][fig_num].set_title(f'Train-{i} in')
+        axs[0][fig_num].set_yticks(list(range(t_in.shape[0])))
+        axs[0][fig_num].set_xticks(list(range(t_in.shape[1])))
+        axs[1][fig_num].imshow(t_out, cmap=cmap, norm=norm)
+        axs[1][fig_num].set_title(f'Train-{i} out')
+        axs[1][fig_num].set_yticks(list(range(t_out.shape[0])))
+        axs[1][fig_num].set_xticks(list(range(t_out.shape[1])))
+        fig_num += 1
+    for i, t in enumerate(task["test"]):
+        t_in = np.array(t["input"])
+        axs[0][fig_num].imshow(t_in, cmap=cmap, norm=norm)
+        axs[0][fig_num].set_title(f'Test-{i} in')
+        axs[0][fig_num].set_yticks(list(range(t_in.shape[0])))
+        axs[0][fig_num].set_xticks(list(range(t_in.shape[1])))
+        fig_num += 1
+    
+    plt.tight_layout()
+    plt.show()  
+
